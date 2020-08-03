76aa205acaf42f8ab164872df42e1689f9a7f971
Andrey <taf_397@yahoo.com>
2020-08-03 12:41:57 +0300

    сщььше

--- views/site/news-modal.php
+++ views/site/news-modal.php
@@ -1,19 +1,19 @@
-<?php if (!empty($news)) :?>
+<?php if (!empty($news)) : ?>
 
     <div class="col-md-10 col-md-offset-1">
         <div class="news">
             <div class="title">
-                <h3 class="text-center"><?=$news->title ; ?></h3>
-                <h4><?=$news->author ; ?><small class="navbar-right date-news"><?=Yii::$app->formatter->asDate("{$news->created_at}", 'long');  ?></small></h4>
+                <h3 class="text-center"><?= $news->title; ?></h3>
+                <h4><?= $news->author; ?><small
+                            class="navbar-right date-news"><?= Yii::$app->formatter->asDate("{$news->created_at}", 'long'); ?></small>
+                </h4>
             </div>
             <div class="news-desription">
-                <?=$news->post;?>
-
-
+                <?= $news->post; ?>
                 <hr>
             </div>
         </div>
     </div>
 <?php else: ?>
-<h2>Такой новости нет</h2>
+    <h2>Такой новости нет</h2>
 <?php endif; ?>
